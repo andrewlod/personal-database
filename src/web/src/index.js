7129ca5cb7e51c89d802095e5a59bde561ace41e
Andre Wlodkovski <andremacwlod@gmail.com>
2026-04-03 15:43:58 -0300

    Add dark mode to UI

--- src/web/src/index.js
+++ src/web/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeWrapper } from './ThemeContext';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeWrapper>
+      <App />
+    </ThemeWrapper>
   </React.StrictMode>
 );
